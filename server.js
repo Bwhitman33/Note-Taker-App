f1ff2e4b1149b33bb15d8f7eeac618a43c845f41
Bwhitman33 <bwhitman33@gmail.com>
2023-05-07 19:40:52 -0400

    Removed some object definitions and reqs from server.js and set up a routes folder for the landing page and notes page.

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
-const fs = require('fs');
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const PORT = process.env.port || 3001;
 const app = express();
-const PORT = 3001;
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
-
-app.get('/api/notes', (req, res) => res.json(notes));
-
-app.post('/api/notes', (req, res) => {
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
